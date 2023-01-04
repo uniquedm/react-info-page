408c9fd490e1d0a070af4b165d774b442289828d
Devansh Mishra <devanshunique@gmail.com>
2023-01-04 23:34:12 +0530

    Moved Files

---
+++ index.js
@@ -0,0 +1,4 @@
+ReactDOM.render(<ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+</ul>, document.getElementById("root"))
--- src/index.js
+++
@@ -1,1 +0,0 @@
-ReactDOM.render(<p>React Static Message</p>, document.getElementById("root"))
