63bacf0bb8464fa20c4bc5a842eaefabce8f5e1a
Devansh Mishra <devanshunique@gmail.com>
2023-01-05 00:20:44 +0530

    Basic JS to JSX

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const h1 = document.createElement('h1');
-h1.textContent = "Hello!"
-h1.className = "header"
-document.getElementById("root").append(h1)
+// const h1 = document.createElement('h1');
+// h1.textContent = "Hello!"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
+
+//JSX
+ReactDOM.render(
+    <h1 className="header">
+        Hello!
+    </h1>,
+    document.getElementById("root")
+)
