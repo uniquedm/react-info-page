ce428f971ac330b8c7667f31b4e1ad8f0851a553
Devansh Mishra <devanshunique@gmail.com>
2023-01-12 23:39:28 +0530

    Made design changes as per Figma Design File

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,12 @@ export default function Main() {
     return (
         <div className="content">
             <h1>Facts about React</h1>
+            <ul className="ul">
+                <li>Released in 2013</li>
+                <li>Originally Created by Jordan Walke</li>
+                <li>Maintained by Facebook</li>
+                <li>Over 100K stars on GitHub</li>
+            </ul>
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function App() {
     return (
         <nav className="nav">
             <img src={logo} alt="logo" className="logo"/>
-            <h3>ReactFacts</h3>
+            <h2 className="logo-text">ReactFacts</h2>
             <h5 className="project">Project - React Info Page</h5>
         </nav>
     );
