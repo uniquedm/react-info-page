180be9f4bfdd9a9fdc4492914ca45d319d782351
Devansh Mishra <devanshunique@gmail.com>
2023-02-21 20:47:06 +0530

    Dark Mode Button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React from "react"
 import logo from "./logo192.png"
 
-export default function App() {
+export default function Navbar() {
+
+    function toggleDarkMode() {
+        console.log("Toggled!")
+    }
+
     return (
         <nav className="nav">
             <img src={logo} alt="logo" className="logo"/>
             <h3 className="logo-text">ReactFacts</h3>
-            <h5 className="project">Project - React Info Page</h5>
+            <button className="dark-mode-button" onClick={toggleDarkMode}>Toggle Dark Mode</button>
         </nav>
     );
 }
