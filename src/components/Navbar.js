293b9dcf11bcb00528adf4ec6a41458942f34c8d
Devansh Mishra <devanshunique@gmail.com>
2023-02-22 20:36:01 +0530

    Basic Dark Mode logic for NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,23 @@ import React from "react"
 import logo from "./logo192.png"
 
 export default function Navbar() {
-
+    let [darkMode, setDarkMode] = React.useState()
     function toggleDarkMode() {
-        console.log("Toggled!")
+        setDarkMode((prevMode)=> !prevMode)
+    }
+
+    let darkStyle = {
+        background: "black",
+        color: "white"
+    }
+
+    let lightStyle = {
+        background: "white",
+        color: "black"
     }
 
     return (
-        <nav className="nav">
+        <nav style={darkMode? darkStyle : lightStyle} className="nav">
             <img src={logo} alt="logo" className="logo"/>
             <h3 className="logo-text">ReactFacts</h3>
             <button className="dark-mode-button" onClick={toggleDarkMode}>Toggle Dark Mode</button>
